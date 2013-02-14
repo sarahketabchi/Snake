3a9a44d9139ee4298e7362b56d90389edf19d44a
Mario+Sarah <mario+sarah@appacademy.io>
2013-02-13 17:47:58 -0800

    Refactored the snakeGame function and moved all snake and board logic out of there.  Added support for a snake to move or turn.

--- snake.js
+++ snake.js
@@ -1,22 +1,65 @@
+// board stuff
+function initializeBoard(size){
+  var grid = [];
+  // Build out columns
+  for (var i = 0; i < size; i++) {
+    var row = new Array(size);
+    grid.push(row);
+  };
+  return grid;
+};
+
+
+// snake stuff
+function initializeSnake() {
+  return {
+    direction: "East",
+    // location: [0, 0],
+    location: { row: 0, col: 0},
+    turn: function turn(dir) {
+      this.direction = dir;
+    },
+    move: function move() {
+      switch (this.direction) {
+        case "North":
+          this.location.row = this.location.row - 1;
+          break;
+        case "East":
+          this.location.col = this.location.col - 1;
+          break;
+        case "South":
+          this.location.row = this.location.row + 1;
+          break;
+        case "West":
+          this.location.col = this.location.col + 1;
+          break;
+      };
+    }
+  };
+};
+
+
+
+
+
+
+
+
+// game stuff
 function snakeGame(gridSize) {
   var board = initializeBoard(gridSize);
-
-
-  function initializeBoard(size){
-    var grid = [];
-    // Build out columns
-    for (var i = 0; i < size; i++) {
-      var row = new Array(size);
-      grid.push(row);
-    };
-    return grid;
-  };
+  var snake =  initializeSnake();
 
   return {
-    board: board
+    board: board,
+    snake: snake
   };
 }
 
+
+
+
+// node stuff
 module.exports = {
   snakeGame: snakeGame
 };
--- snakeTest.js
+++ snakeTest.js
@@ -3,4 +3,11 @@ function s(game) { console.log(game) };
 
 var game = Snake.snakeGame(10);
 
-s(game.board);
+// s(game.board);
+// s(game.snake.location);
+s(game.snake.direction);
+game.snake.turn("South");
+s(game.snake.direction);
+s(game.snake.location);
+game.snake.move();
+s(game.snake.location);
